acc21c32a8e4468c162c84febabf0bb973121d83
Hello1102 <155940545+Hello1102@users.noreply.github.com>
2024-05-26 19:07:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "db98eba424dfb1755f72db5fa71785e1"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "7276779905:AAGUtp59GD4I6psSQ9DIWpm01eNwuDmWkG8"
+OWNER_ID = "6713421664"
 SUPPORT_GRP = "suppooorttt"
 UPDATE_CHNL = "suppooorttt"
 OWNER_USERNAME = "Mr_nobi_1"
