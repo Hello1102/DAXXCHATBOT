febf67620f77ff6668f55e3f80ac1fbc0294bcd9
Hello1102 <155940545+Hello1102@users.noreply.github.com>
2024-06-18 19:50:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "29232955"
+API_ID = "28772940"
 # -------------------------------------------------------------
-API_HASH = "db98eba424dfb1755f72db5fa71785e1"
+API_HASH = "71c581b51ae9c93c62a8610756294ec1"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = "6713421664"
-SUPPORT_GRP = "suppooorttt"
-UPDATE_CHNL = "suppooorttt"
+SUPPORT_GRP = "Mr_nobi_1"
+UPDATE_CHNL = "Mr_nobi_1"
 OWNER_USERNAME = "Mr_nobi_1"
 
